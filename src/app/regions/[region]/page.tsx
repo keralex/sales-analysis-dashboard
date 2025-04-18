7a33224e647092beb8bbd1695092e26c978de250
keralex <kerlis.aguado26@gmail.com>
2025-04-18 17:55:33 -0300

    fix types

--- src/app/regions/[region]/page.tsx
+++ src/app/regions/[region]/page.tsx
@@ -1,15 +1,9 @@
 import RegionPerformance from "@/components/region-performance"
 
-interface RegionDetailPageProps {
-    params: {
-        region: string
-    }
-}
 
-export default async function RegionDetailPage({ params }: RegionDetailPageProps) {
-    // Decode the region name from the URL
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default async function RegionDetailPage(params: any) {
     const regionName = decodeURIComponent(params.region)
-
     return (
         <div className="flex flex-col h-full">
             <header className="bg-white dark:bg-gray-800 shadow-sm p-4 md:p-6">
